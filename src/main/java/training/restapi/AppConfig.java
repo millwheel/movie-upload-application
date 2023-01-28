034881b750aed4670b98442af35a3a7156e41e57
Sijun <sionwer4@naver.com>
2023-01-28 10:51:28 +0900

    feat(home): Distinguish login user at home

--- src/main/java/training/restapi/AppConfig.java
+++ src/main/java/training/restapi/AppConfig.java
@@ -1,10 +1,12 @@
 package training.restapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
 @Configuration
 public class AppConfig {
+
 
 }
--- src/main/java/training/restapi/controller/HomeController.java
+++ src/main/java/training/restapi/controller/HomeController.java
@@ -1,14 +1,30 @@
 package training.restapi.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import training.restapi.domain.Member;
 
+@Slf4j
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
-        return "welcome";
+    public String home(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        if(session == null){
+            log.info("no session");
+            return "welcome";
+        }
+        Member loginMember = (Member)session.getAttribute("loginMember");
+        if(loginMember == null){
+            log.info("no user info");
+            return "welcome";
+        }
+
+        return "welcome, member!";
     }
 
 }
