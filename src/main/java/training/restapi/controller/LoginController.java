c8e372451530a7939adc30c5aa0020aa465905e3
Sijun <sionwer4@naver.com>
2023-02-05 20:16:21 +0900

    feat(interceptor): Add redirect function to login page when non-login user access

--- src/main/java/training/restapi/controller/LoginController.java
+++ src/main/java/training/restapi/controller/LoginController.java
@@ -5,10 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import training.restapi.Const;
 import training.restapi.domain.Member;
 import training.restapi.form.LoginForm;
@@ -38,6 +35,7 @@ public class LoginController {
     @PostMapping("/login")
     public void login(
             @RequestBody LoginForm data,
+            @RequestParam(defaultValue = "/") String redirectURL,
             HttpServletRequest request,
             HttpServletResponse response
     ) throws IOException {
@@ -54,7 +52,7 @@ public class LoginController {
         log.info("login success");
         HttpSession session = request.getSession();
         session.setAttribute(Const.LOGIN_MEMBER, loginMember);
-        response.sendRedirect("/");
+        response.sendRedirect(redirectURL);
     }
 
     @PostMapping("/logout")
--- src/main/java/training/restapi/interceptor/LoginCheckInterceptor.java
+++ src/main/java/training/restapi/interceptor/LoginCheckInterceptor.java
@@ -11,9 +11,13 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        log.info("certification activated. access to URI={}",requestURI);
         HttpSession session = request.getSession();
+
         if (session == null || session.getAttribute("loginMember") == null){
             log.info("non-certified access");
+            response.sendRedirect("/login?redirect=" + requestURI);
             return false;
         }
         return true;
