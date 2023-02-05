02f71425ef7dcc509c3874aee8497ec76bac7686
Sijun <sionwer4@naver.com>
2023-02-05 12:29:29 +0900

    feat: All spring interceptor to all applicatoin except for login and join

---
+++ src/main/java/training/restapi/WebConfig.java
@@ -0,0 +1,18 @@
+package training.restapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import training.restapi.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/join", "/login", "logout");
+    }
+}
