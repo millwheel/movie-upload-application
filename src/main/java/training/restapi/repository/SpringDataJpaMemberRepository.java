98cca758359f0d40d93b905e5bf6dc384620e753
Sijun <sionwer4@naver.com>
2023-02-05 13:50:14 +0900

    refactor(repository): Rename spring data jpa repository to use it for video repository

--- src/main/java/training/restapi/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/training/restapi/repository/SpringDataJpaMemberRepository.java
@@ -1,12 +1,37 @@
 package training.restapi.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
 import training.restapi.domain.Member;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long> {
+@Slf4j
+@Repository
+@Primary
+@Transactional
+public class SpringDataJpaMemberRepository implements MemberRepository {
 
-    Optional<Member> findByEmail(String email);
+    private final SpringDataJpaRepository repository;
+
+    public SpringDataJpaMemberRepository(SpringDataJpaRepository springDataJpaRepository) {
+        this.repository = springDataJpaRepository;
+    }
+
+    @Override
+    public void make(Member member) {
+        repository.save(member);
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
 }
