be1ec542df7a623d62fb47aaa196b2b4e92731ed
Sijun <sionwer4@naver.com>
2023-02-06 17:22:34 +0900

    feat: Create file upload function by using MultipartFile

--- src/main/java/training/restapi/controller/VideoController.java
+++ src/main/java/training/restapi/controller/VideoController.java
@@ -4,22 +4,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import training.restapi.Const;
 import training.restapi.domain.Member;
 import training.restapi.domain.Video;
-import training.restapi.form.LoginForm;
 import training.restapi.form.VideoForm;
 import training.restapi.service.MemberService;
 import training.restapi.service.VideoService;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @Slf4j
 @RestController
 public class VideoController {
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     private final VideoService videoService;
     private final MemberService memberService;
@@ -30,7 +35,7 @@ public class VideoController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/video-list")
+    @GetMapping("/video/list")
     public List<Video> videoList(HttpServletRequest request){
         HttpSession session = request.getSession();
         Member loginMember = (Member)session.getAttribute(Const.LOGIN_MEMBER);
@@ -41,12 +46,22 @@ public class VideoController {
         return videoList;
     }
 
-    @PostMapping("/video/add")
-    public void videoAdd(@RequestBody VideoForm data, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @PostMapping("/video/upload")
+    public void videoAdd(
+            @RequestPart MultipartFile file,
+            @RequestPart VideoForm videoForm,
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         HttpSession session = request.getSession();
         Member loginMember = (Member)session.getAttribute(Const.LOGIN_MEMBER);
-        videoService.saveVideo(data.getName(), loginMember.getName());
-        response.sendRedirect("/video-list");
+        videoService.saveVideo(videoForm.getName(), loginMember.getName());
+
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            file.transferTo(new File(fullPath));
+        }
+
+        response.sendRedirect("/video/list");
     }
 
 }
